39698aa224dedf84f8c92eb292f301df91d01362
Vikash Kumar <vk2372625@gmail.com>
2026-08-15 10:12:30 +0530

    Day2

---
+++ Leetcode3702.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int longestSubsequence(int[] nums) {
+        int xor = 0;
+        boolean Allzero = true;
+        for(int ele:nums){
+            xor = xor ^ ele;
+            if(ele != 0){
+                Allzero =false;
+            }
+        }
+        if(xor !=0) return nums.length;
+        if(Allzero) return 0;
+        return nums.length -1;
+    }
+}
